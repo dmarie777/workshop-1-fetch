3f10d19bae04bf7c5c8d83f55cd03bb40f679e91
dmarie777 <deimarie777@gmail.com>
2024-03-02 17:35:29 -0500

    Added content inside the elements

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 
-const url = "https://platzi-avo.vercel.app/api/avo";
+const baseUrl = "https://platzi-avo.vercel.app";
 // web api
-fetch(url)
+fetch(`${baseUrl}/api/avo`)
 .then(res => res.json())
 .then(data => {
     const allElements = [];
     data.data.forEach(element => {
         //create elements
         const image = document.createElement('img');
+        image.src = `${baseUrl}${element.image}`;
         const title = document.createElement('h2');
+        title.textContent = element.price;
         const price = document.createElement('div');
+        price.textContent = element.name;
         //create container
         const container = document.createElement('div');
         //append elements
