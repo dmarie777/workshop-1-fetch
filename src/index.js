b958b80bd95db34dea609ecb9eb3d38b8e44ec01
dmarie777 <deimarie777@gmail.com>
2024-03-02 15:32:37 -0500

    Added elements to the DOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
-console.log('Happy hacking :)')
+const url = "https://platzi-avo.vercel.app/api/avo";
+// web api
+fetch(url)
+.then(res => res.json())
+.then(data => {
+    const allElements = [];
+    data.data.forEach(element => {
+        //create elements
+        const image = document.createElement('img');
+        const title = document.createElement('h2');
+        const price = document.createElement('div');
+        //create container
+        const container = document.createElement('div');
+        //append elements
+        container.append(image, title, price);
+        //pushear container to all elements
+        allElements.push(container);
+    });
+    //append elements from allElements
+    document.body.append(...allElements);
+});
